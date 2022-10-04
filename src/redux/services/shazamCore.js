1067a42aab1f4c3f34663a26048d135b3c1d3012
bartek2397 <bartosz.wszolek97@gmail.com>
2022-10-04 15:23:58 +0200

    preparing redux, discover component

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+    export const shazamCoreApi = createApi({
+        reducerPath: 'shazamCoreApi',
+        baseQuery: fetchBaseQuery({
+            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+            prepareHeaders: () => {
+                headers.set('X-RapidAPI-Key', 'c03a2e4613mshaec2909cdce0fd3p19f1a8jsn62c4a0c4c971')
+            }
+        })
+    })
